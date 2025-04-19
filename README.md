0173941a60414b1a090a353ac288f6b6a0cc9c12
nizmitz <nizmitz@gmail.com>
2025-04-19 19:16:45 +0700

    terraform file for testing

---
+++ README.md
@@ -0,0 +1,1 @@
+# poc-doc-sql-datacache
---
+++ main.tf
@@ -0,0 +1,71 @@
+provider "google" {
+  project = "agnes-learning-project-3"
+  region  = "asia-southeast2"
+}
+
+provider "google-beta" {
+  project = "agnes-learning-project-3"
+  region  = "asia-southeast2"
+  zone    = "asia-southeast2-a"
+}
+
+
+resource "google_sql_database_instance" "cache_enabled_ent_plus" {
+  name             = "cache-enabled-ent-plus"
+  database_version = "POSTGRES_17"
+  region           = "asia-southeast2"
+
+  settings {
+    tier    = "db-perf-optimized-N-2"
+    edition = "ENTERPRISE_PLUS"
+    data_cache_config {
+      data_cache_enabled = true
+    }
+  }
+}
+
+resource "google_sql_database_instance" "cache_disabled_ent_plus" {
+  name             = "cache-disabled-ent-plus"
+  database_version = "POSTGRES_17"
+  region           = "asia-southeast2"
+
+  settings {
+    tier    = "db-perf-optimized-N-2"
+    edition = "ENTERPRISE_PLUS"
+  }
+}
+
+resource "google_sql_database_instance" "cache_disabled_ent" {
+  name             = "cache-disabled-ent"
+  database_version = "POSTGRES_17"
+  region           = "asia-southeast2"
+
+  settings {
+    tier    = "db-f1-micro"
+    edition = "ENTERPRISE"
+  }
+}
+
+output "cache_enabled_ent_plus" {
+  value = {
+    edition           = google_sql_database_instance.cache_enabled_ent_plus.settings.0.edition
+    tier              = google_sql_database_instance.cache_enabled_ent_plus.settings.0.tier
+    data_cache_config = google_sql_database_instance.cache_enabled_ent_plus.settings.0.data_cache_config
+  }
+}
+
+output "cache_disabled_ent_plus" {
+  value = {
+    edition           = google_sql_database_instance.cache_disabled_ent_plus.settings.0.edition
+    tier              = google_sql_database_instance.cache_disabled_ent_plus.settings.0.tier
+    data_cache_config = google_sql_database_instance.cache_disabled_ent_plus.settings.0.data_cache_config
+  }
+}
+
+output "cache_disabled_ent" {
+  value = {
+    edition           = google_sql_database_instance.cache_disabled_ent.settings.0.edition
+    tier              = google_sql_database_instance.cache_disabled_ent.settings.0.tier
+    data_cache_config = google_sql_database_instance.cache_disabled_ent.settings.0.data_cache_config
+  }
+}
