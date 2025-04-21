ccb47d6408e9e0f3a890b568064747eeb9c14b18
nizmitz <fattahemiryanuar@gmail.com>
2025-04-21 19:18:09 +0700

    add testing for SQL Server

--- main.tf
+++ main.tf
@@ -9,11 +9,33 @@ provider "google-beta" {
   zone    = "asia-southeast2-a"
 }
 
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
 
 resource "google_sql_database_instance" "cache_enabled_ent_plus" {
   name             = "cache-enabled-ent-plus"
   database_version = "POSTGRES_17"
   region           = "asia-southeast2"
+  deletion_protection = false
+
+  settings {
+    tier    = "db-perf-optimized-N-2"
+    edition = "ENTERPRISE_PLUS"
+    data_cache_config {
+      data_cache_enabled = true
+    }
+  }
+}
+
+resource "google_sql_database_instance" "cache_enabled_ent_plus_sql_server" {
+  name             = "cache-enabled-ent-plus"
+  database_version = "SQLSERVER_2022_ENTERPRISE"
+  region           = "asia-southeast2"
+  deletion_protection = false
+  root_password = random_password.password.result
 
   settings {
     tier    = "db-perf-optimized-N-2"
@@ -28,6 +50,7 @@ resource "google_sql_database_instance" "cache_null_ent_plus" {
   name             = "cache-null-ent-plus"
   database_version = "POSTGRES_17"
   region           = "asia-southeast2"
+  deletion_protection = false
 
   settings {
     tier    = "db-perf-optimized-N-2"
@@ -39,6 +62,7 @@ resource "google_sql_database_instance" "cache_disabled_ent_plus" {
   name             = "cache-disabled-ent-plus"
   database_version = "POSTGRES_17"
   region           = "asia-southeast2"
+  deletion_protection = false
 
   settings {
     tier    = "db-perf-optimized-N-2"
@@ -54,6 +78,7 @@ resource "google_sql_database_instance" "cache_disabled_ent" {
   name             = "cache-disabled-ent"
   database_version = "POSTGRES_17"
   region           = "asia-southeast2"
+  deletion_protection = false
 
   settings {
     tier    = "db-f1-micro"
@@ -67,7 +92,15 @@ output "cache_enabled_ent_plus" {
     tier              = google_sql_database_instance.cache_enabled_ent_plus.settings.0.tier
     data_cache_config = google_sql_database_instance.cache_enabled_ent_plus.settings.0.data_cache_config
   }
-}
+ }
+
+output "cache_enabled_ent_plus_sql_server" {
+  value = {
+    edition           = google_sql_database_instance.cache_enabled_ent_plus_sql_server.settings.0.edition
+    tier              = google_sql_database_instance.cache_enabled_ent_plus_sql_server.settings.0.tier
+    data_cache_config = google_sql_database_instance.cache_enabled_ent_plus_sql_server.settings.0.data_cache_config
+  }
+ }
 
 output "cache_null_ent_plus" {
   value = {
